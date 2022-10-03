8a2f7fb66741cf022aa3acd1e8c7a208210f4bd6
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2022-10-03 14:35:38 -0600

    Added in the Engine project from GAT150

---
+++ Engine/Framework/event.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <functional>
+#include <variant>
+
+namespace vl
+{
+	class GameObject;
+
+	class Event
+	{
+	public:
+		using functionPtr = std::function<void(const Event&)>;
+
+	public:
+		std::string name;
+		GameObject* reciever{ nullptr };
+		std::variant<int, bool, float, std::string> data;
+
+	};
+
+	class INotify
+	{
+	public:
+		virtual void OnNotify(const Event& event) = 0;
+	};
+}
