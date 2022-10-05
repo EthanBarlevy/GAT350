b77809b535fd0b8a92694a6a82c0edf7921c14d4
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2022-10-05 16:22:45 -0600

    mostly just notes

--- Application/mian.cpp
+++ Application/mian.cpp
@@ -10,6 +10,9 @@ int main(int argc, char** argv)
 
 	vl::g_renderer.CreateWindow("Application", 800, 600);
 
+	float angle = 0;
+	vl::Vector2 position;
+
 	bool quit = false;
 	while (!quit)
 	{
@@ -19,16 +22,37 @@ int main(int argc, char** argv)
 
 		vl::g_renderer.BeginFrame();
 
+
+
+
+		if (vl::g_inputSystem.GetKeyState(vl::key_left) == vl::InputSystem::State::Held) 
+		{ 
+			position.x -= vl::g_time.deltaTime; 
+		}
+		if (vl::g_inputSystem.GetKeyState(vl::key_right) == vl::InputSystem::State::Held)
+		{
+			position.x += vl::g_time.deltaTime;
+		}
+		glPushMatrix();
+
+		angle += 45.0f * (float)vl::g_time.deltaTime;
+
+		//glRotatef(angle , 0.0f, 0.0f, 1.0f);
+		glScalef(0.5f, 0.5f, 0.5f);
+		glTranslatef(position.x, position.y, 0);
+
 		glBegin(GL_TRIANGLES);
 
 		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex2f(-0.5f, -0.5f);
-		glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex2f(0.0f, 0.5f);
-		glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex2f(0.5f, -0.5f);
+		glVertex2f(-1.0f, -1.0f);
+		//glColor3f(0.0f, 1.0f, 0.0f);
+		glVertex2f(0.0f, 1.0f);
+		//glColor3f(0.0f, 0.0f, 1.0f);
+		glVertex2f(1.0f, -1.0f);
 
 		glEnd();
+
+		glPopMatrix();
 
 		vl::g_renderer.EndFrame();
 	}
