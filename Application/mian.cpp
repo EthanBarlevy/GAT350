6f9d36b24cf1b8425f1901373c051bb2588a067b
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2022-10-06 16:25:50 -0600

    mostly just updating to newer opengl

--- Application/mian.cpp
+++ Application/mian.cpp
@@ -1,6 +1,31 @@
 #include "engine.h"
 #include <iostream>
 
+float points[] = {
+  -0.5f, -0.5f,  0.0f,
+   0.5f, -0.5f,  0.0f,
+   0.5f,  0.5f,  0.0f,  
+   -0.5f, -0.5f,  0.0f,
+   0.5f,  0.5f,  0.0f,
+   -0.5f, 0.5f,  0.0f
+};
+
+const char* vertex_shader =
+"#version 430 core\n"
+"in vec3 position;"
+"void main() {"
+"  gl_Position = vec4(position * 1.5, 1.0);"
+"}";
+
+
+
+const char* fragment_shader =
+"#version 430 core\n"
+"out vec4 color;"
+"void main() {"
+"  color = vec4(1.0, 1.0, 1.0, 1.0);"
+"}";
+
 int main(int argc, char** argv)
 {
 	vl::InitializeMemory();
@@ -10,8 +35,34 @@ int main(int argc, char** argv)
 
 	vl::g_renderer.CreateWindow("Application", 800, 600);
 
-	float angle = 0;
-	vl::Vector2 position;
+	// create vertex buffer (will probably move later)
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, 18 * sizeof(float), points, GL_STATIC_DRAW);
+
+	// create vertex array
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+	// create shader
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertex_shader, NULL);
+	glCompileShader(vs);
+	GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragment_shader, NULL);
+	glCompileShader(fs);
+
+	// create program
+	GLuint program = glCreateProgram();
+	glAttachShader(program, fs);
+	glAttachShader(program, vs);
+	glLinkProgram(program);
+	glUseProgram(program);
 
 	bool quit = false;
 	while (!quit)
@@ -22,37 +73,7 @@ int main(int argc, char** argv)
 
 		vl::g_renderer.BeginFrame();
 
-
-
-
-		if (vl::g_inputSystem.GetKeyState(vl::key_left) == vl::InputSystem::State::Held) 
-		{ 
-			position.x -= vl::g_time.deltaTime; 
-		}
-		if (vl::g_inputSystem.GetKeyState(vl::key_right) == vl::InputSystem::State::Held)
-		{
-			position.x += vl::g_time.deltaTime;
-		}
-		glPushMatrix();
-
-		angle += 45.0f * (float)vl::g_time.deltaTime;
-
-		//glRotatef(angle , 0.0f, 0.0f, 1.0f);
-		glScalef(0.5f, 0.5f, 0.5f);
-		glTranslatef(position.x, position.y, 0);
-
-		glBegin(GL_TRIANGLES);
-
-		glColor3f(1.0f, 0.0f, 0.0f);
-		glVertex2f(-1.0f, -1.0f);
-		//glColor3f(0.0f, 1.0f, 0.0f);
-		glVertex2f(0.0f, 1.0f);
-		//glColor3f(0.0f, 0.0f, 1.0f);
-		glVertex2f(1.0f, -1.0f);
-
-		glEnd();
-
-		glPopMatrix();
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		vl::g_renderer.EndFrame();
 	}
